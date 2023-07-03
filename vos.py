e983fc56bde3e2804d4358fbf45c2a140841b147
Elchin03 <elcin03@mail.ru>
2023-07-03 18:22:25 +0300

    prosimvosim

--- vos.py
+++ vos.py
@@ -1,8 +1,9 @@
 import re
 
+
 def mumu(x):
     r = r"array\(\s*([^)]*)\)\s*=>\s*([^.]*)"
-    z = re.findall(r,x)
+    z = re.findall(r, x)
     ls2 = []
     for ints, key in z:
         ls = []
@@ -13,6 +14,9 @@ def mumu(x):
             ls.append(i)
         p = (key, ls)
         ls2.append(p)
-        print(ints,key)
+        print(ints, key)
     return ls2
-print(mumu(".do | glob array( tear . usraza_742 . aes_561 ) => aqulete_707.|;|glob array( ussote_180 . arbila ) => ordi_963. |; |glob array(arsoare . ate ) => isrete. |; .end))"))
+
+
+print(mumu(
+    ".do | glob array( tear . usraza_742 . aes_561 ) => aqulete_707.|;|glob array( ussote_180 . arbila ) => ordi_963. |; |glob array(arsoare . ate ) => isrete. |; .end))"))
