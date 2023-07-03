956d4e77c563859a9f34c5ab6c9f7fc2274046f8
Elchin03 <elcin03@mail.ru>
2023-07-03 18:48:19 +0300

    prosimvosim

--- vosemi.py
+++ vosemi.py
@@ -1,14 +1,13 @@
 import re
 
 def m(s):
-    r = "q\(([^)]*\))\s*:\s*#([^ ]*)"
+    r = r"q\(([^)]*\))\s*:\s*#([^ ]*)"
     z= re.findall(r,s)
-    print(z)
     l2=[]
     # for s1, ints in z:
     #     l=(s1,ints)
     #     l2.append(l)
     # return l2
-
+    return z
 print(m("<block> begin local q(zaxedi_23) : #8577 end; begin local q(lemaan_61) : #8852 end; begin local q(inri): #-6505 end;</block>"))
 
